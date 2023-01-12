1dc71e79dd47d9274ebe58b4f8bf8ddd9a5afdb4
Frank Nguyen <frank@ultra.me>
2023-01-11 19:04:03 -0800

    add ability to view all employees using await promise

--- index.js
+++ index.js
@@ -1,27 +1,70 @@
 const inquirer = require('inquirer');
-const db = require('./config/connection')
+// const db = require('./config/connection')
+const mysql = require('mysql2/promise')
+const cTable = require('console.table');
+const { existsSync } = require('fs');
+const { exit } = require('process');
 
-
+require('dotenv').config();
 
 const questions = [
-  {
-    type: 'expand',
-    name: 'menu',
-    message: "What would you like to do?",
-    choices: [
-      'View All Employees', 
-      'Add Employee', 
-      'Update Employee Role',
-      'View All Roles',
-      'Add Role',
-      'View All Departments',
-      'Add Department',
-      'Quit'
-    ]
-  }
+ 	 {
+		type: 'list',
+		name: 'option',
+		message: "What would you like to do?",
+		choices: [
+		'View All Employees', 
+		'Add Employee', 
+		'Update Employee Role',
+		'View All Roles',
+		'Add Role',
+		'View All Departments',
+		'Add Department',
+		'Quit'
+    	]
+  	}
 ]
 
-// choose view all employees
-// invoke select * from employees
+async function promptUser() {
+	while(true) {
+		// create the connection
+		const connection = await mysql.createConnection(
+			{
+				user: process.env.DB_USER,
+				database: process.env.DB_DB,
+				password:process.env.DB_PASS,
+				host: process.env.DB_HOST,
+			},
+				console.log(`Connected to the employee_db database.`)
+		);
+		
+		const answers = await inquirer.prompt([
+			{
+				type: 'list',
+				name: 'option',
+				message: "What would you like to do?",
+				choices: [
+				'View All Employees', 
+				'Add Employee', 
+				'Update Employee Role',
+				'View All Roles',
+				'Add Role',
+				'View All Departments',
+				'Add Department',
+				'Quit'
+					]
+			  }
+		]);
+			if (answers.option === 'Quit') {
+				console.log('Quitting');
+				exit();
+			}
+			if (answers.option === 'View All Employees') {
+				const [rows] = await connection.execute('SELECT * FROM employee');
+				console.table(rows);
+			}
+		connection.end();
+	}
+}
 
-inquirer.prompt(questions)
+promptUser()
