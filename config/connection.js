fd517875ca8265fff0504275e791a87f504aba3e
Frank Nguyen <frank@ultra.me>
2023-01-11 16:27:53 -0800

    fixed an issue that was causing seeds file to not execute

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,34 @@
 const mysql = require('mysql2')
+const fs = require("fs")
+
+// load .env variables
 require('dotenv').config();
+
+// read sql seed query
+const seedQuery = fs.readFileSync('./db/seeds.sql', {
+  encoding: "utf-8",
+})
+
+// connect to database
 const db = mysql.createConnection(
   {
-    user: process.env.DB_DB,
-    database: process.env.DB_USER,
+    user: process.env.DB_USER,
+    database: process.env.DB_DB,
     password:process.env.DB_PASS,
     host: process.env.DB_HOST,
   },
     console.log(`Connected to the employee_db database.`)
 );
+
+db.connect();
+console.log("Running SQL seed...")
+
+db.query(seedQuery, err => {
+  if(err) {
+    throw err
+  }
+  console.log("SQL seed completed")
+  db.end();
+})
 
 module.exports = db;
---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (department_name)
+    VALUES ("Sales"),
+           ("Engineering"),
+           ("Finance"),
+           ("Legal");
+
+INSERT INTO roles (title, salary, department_id)
+    VALUES ("Sales Lead", 100000, 1),
+           ("Salesperson", 80000, 1),
+           ("Lead Engineer", 150000, 2),
+           ("Software Engineer", 120000, 2),
+           ("Account Manager", 160000, 3),
+           ("Accountant", 125000, 3),
+           ("Legal Team Lead", 250000, 4),
+           ("Lawyer", 190000, 4);
+
+
+INSERT INTO employee ( first_name, last_name, role_id, manager_id)
+    VALUES ("John", "Smith", 1, null),
+           ("Ben", "Thomas", 2, 1),
+           ("Jenny", "Rothlin", 3, null),
+           ("Mark", "Anthony", 4, 3),
+           ("Ken", "Shelby", 5, null),
+           ("Sam", "Williams", 6, 5),
+           ("Natasha", "Vogel", 7, null),
+           ("Robert", "Barnes", 8, 7),
+
+
