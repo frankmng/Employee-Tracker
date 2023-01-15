eb006d00c1126581901e2c1621173f1132d9df7b
Frank Nguyen <frank@ultra.me>
2023-01-14 17:22:59 -0800

    added bonus function to view total budget by department

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,7 +14,6 @@ INSERT INTO roles (title, salary, department_id)
            ("Legal Team Lead", 250000, 4),
            ("Lawyer", 190000, 4);
 
-
 INSERT INTO employee ( first_name, last_name, role_id, manager_id)
     VALUES ("John", "Smith", 1, null),
            ("Ben", "Thomas", 2, 1),
--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function promptUser() {
 				'Add Role',
 				'View All Departments',
 				'Add Department',
+				'View Total Budget by Department',
 				'Quit'
 					]
 			  }
@@ -41,7 +42,7 @@ async function promptUser() {
 			// view all employees
 			if (answers.option === 'View All Employees') {
 				const [rows] = await connection.execute
-					('with employeeData as (SELECT e.id as ID, e.first_name as FirstName, e.last_name as LastName, r.title as Title, d.department_name as Department, r.salary as Salary, e.manager_id from employee e join roles r on e.role_id = r.id join department d on r.department_id = d.id) SELECT ed.ID, ed.FirstName, ed.LastName, ed.Title, ed.Department, ed.Salary, e.first_name as Manager from employeeData ed left join employee e on ed.manager_id = e.id');
+					('with employeeData as (SELECT e.id as ID, e.first_name as FirstName, e.last_name as LastName, r.title as Title, d.department_name as Department, r.salary as Salary, e.manager_id from employee e join roles r on e.role_id = r.id join department d on r.department_id = d.id) SELECT ed.ID, ed.FirstName, ed.LastName, ed.Title, ed.Department, ed.Salary, CONCAT(e.first_name, " ", e.last_name) as Manager from employeeData ed left join employee e on ed.manager_id = e.id');
 					console.table(rows);
 			}
 			// view all roles
@@ -110,9 +111,6 @@ async function promptUser() {
 
 					const role_id = roleID[0][0].id;
 					const manager_id = managerID[0][0].id;
-
-					console.log(typeof(role_id));
-					console.log(manager_id);
 					  
 					const addEmployee = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${firstName}', '${lastName}', '${role_id}', '${manager_id}')`;
 					await connection.execute(addEmployee);
@@ -166,9 +164,75 @@ async function promptUser() {
 				const updateEmployee = `UPDATE employee SET role_id = '${role_id}' WHERE id = ${emp_id};`;
 				await connection.execute(updateEmployee);
 				console.log("Updated employee's role");
-				
+			}
 
+			if (answers.option === 'Add Department') {
+				let departmentData = await inquirer.prompt([
+					{
+						type: 'input',
+						name: 'department',
+						message: "What is the name of the department?"
+					}
+				])
+				const {department} = departmentData
+				const addDepartment = `INSERT INTO department (department_name) VALUES ('${department}')`;
+				await connection.execute(addDepartment);
+				console.log(`Added ${department} to the database`);
+			}
 
+			if (answers.option === 'Add Role') {
+				let roleData = await inquirer.prompt([
+					{
+						type: 'input',
+						name: 'role',
+						message: "What is the name of the role?"
+					},
+					{
+						type: 'input',
+						name: 'salary',
+						message: "What is the salary of the role?"
+					},
+					{
+						type: 'list',
+						name: 'department',
+						message: "What department does this role belong to?",
+						choices: 
+						[
+							'Sales', 
+							'Engineering',
+							'Finance',
+							'Legal'
+						]
+					}
+				])
+				const {role, salary, department} = roleData
+				const departmentID = await connection.execute(`SELECT id from department where department_name like '${department}'`) 
+				const department_id = departmentID[0][0].id;
+
+				const addRole= `INSERT INTO roles (title, salary, department_id) VALUES ('${role}', '${salary}', '${department_id}')`;
+				await connection.execute(addRole);
+				console.log(`Added ${role} to the database`);
+			}
+			if (answers.option === 'View Total Budget by Department') {
+				let totalBudget = await inquirer.prompt([
+					{
+						type: 'list',
+						name: 'department',
+						message: "Which department do you want to view the total budget for?",
+						choices: 
+						[
+							'Sales', 
+							'Engineering',
+							'Finance',
+							'Legal'
+						]
+					}
+				])
+				const {department} = totalBudget
+				const departmentBudget = await connection.execute(`SELECT sum(salary) as budget from department d join roles r on d.id = r.department_id join employee e on e.role_id = r.id where department_name like '%${department}%'`)
+				const department_budget = departmentBudget[0][0].budget;
+
+				console.log(`The total budget utilized for the ${department} department is $${department_budget}`)
 			}
 		connection.end();
 	}
